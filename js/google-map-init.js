6f8f8d689b145637442ebdaeb3ee4af295fe8745
mathewthomason <mathewthomason@outlook.com>
2017-05-03 12:13:35 -0400

    map v6

--- js/google-map-init.js
+++ js/google-map-init.js
@@ -6,8 +6,6 @@ jQuery(document).ready(function() {
         var a = {
                 zoom: 11,
                 scrollwheel: false,
-           <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDwEcC9l18gidedyipKJb3gA9BOxzG8jU&callback=initMap"
-  type="text/javascript"></script>
                 center: new google.maps.LatLng(35.102426,-92.311857),
                 styles: [{
                     "featureType": "landscape",
@@ -135,5 +133,4 @@ jQuery(document).ready(function() {
         })
     }
     google.maps.event.addDomListener(window, "load", b);
-
 });
