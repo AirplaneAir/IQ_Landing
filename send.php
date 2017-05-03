5e53b20b2d53feca64dcce5e94e0f3b875db558d
mathewthomason <mathewthomason@outlook.com>
2017-05-03 17:57:46 -0400

    index flow fix

--- send.php
+++ send.php
@@ -8,5 +8,7 @@ $subject = "New Inquiry"
 
 mail ($to, $subject, $message, "From " . $full_name . $email);
 
+alert("Thanks for filling out a contact fourm. You will be contacted within 24-48 hours");
+window.location.replace("http://webistetitle.com")
 
 ?>
